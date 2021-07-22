67e585b1370ec5f3b679aae43f804204e94e497f
badcwmeng <badcwmeng@tencent.com>
2021-07-22 16:10:33 +0800

    build pass benchmark

---
+++ compare/gen.cc
@@ -0,0 +1,5 @@
+//
+// Created by badcw on 2021/7/22.
+//
+
+#include "gen.h"
---
+++ compare/gen.h
@@ -0,0 +1,14 @@
+//
+// Created by badcw on 2021/7/22.
+//
+
+#ifndef ICPC_GEN_H
+#define ICPC_GEN_H
+
+
+class gen {
+
+};
+
+
+#endif //ICPC_GEN_H
