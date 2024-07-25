3091b4fdbaea2767d5ae9d495812ce7863dc2247
WUST-mengqinyu <badcw123@gmail.com>
2024-07-25 20:35:32 +0800

    rewrite bundler with rust

---
+++ bundle/src/consts.rs
@@ -0,0 +1,6 @@
+#![allow(dead_code)]
+pub static PROJECT_DIR: &'static str = "/home/mengqinyu.01/repo/icpc-cmake";
+pub static ICPC_EXECUTABLE_LIST: &'static str = "bin_a;main";
+pub static ICPC_MAIN_SOURCE: &'static str = "/home/mengqinyu.01/repo/icpc-cmake/bin/a.cc;/home/mengqinyu.01/repo/icpc-cmake/main.cc";
+pub static BUNDLE_OUT_DIR: &'static str = "/home/mengqinyu.01/repo/icpc-cmake/bin_output";
+pub static EXCEPT_DEF: &'static str = "badcw";
--- main.cc
+++ main.cc
@@ -1,9 +1,4 @@
-/*================================================================
-*
-*   创 建 者： badcw
-*   创建日期： 2024-07-19 23:27:36
-*
-================================================================*/
+#include "inner/number"
 #include "inner/prelude"
 
 #define VI vector<int>
@@ -16,10 +11,12 @@ const int mod = 1e9 + 7;
 int f[maxn][10];
 
 int main(int argc, char **agrv) {
+  inner::num::init(100);
+  W(inner::num::factor(10));
   TKASE {
     int a;
     R(a);
-    W(NoSuffix{"case #"}, kase, inner::qp<mod>(2, a));
+    W(NoSuffix("case #"), kase, inner::qp<mod>(22, a));
   }
   return 0;
 }
