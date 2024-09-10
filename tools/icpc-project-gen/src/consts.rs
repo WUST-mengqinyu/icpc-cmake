55f6f505829ae1a82ca42b40191812cc4f17dc91
badcw <badcw123@gmail.com>
2024-09-11 00:07:13 +0800

    del local file

--- tools/icpc-project-gen/src/consts.rs
+++
@@ -1,6 +0,0 @@
-#![allow(dead_code)]
-pub static PROJECT_DIR: &str = "/home/badcw/repo/icpc-cmake";
-pub static ICPC_EXECUTABLE_LIST: &str = "bin_A;bin_B;bin_C;bin_D;bin_E;bin_F;bin_G;bin_H;bin_tmp;main";
-pub static ICPC_MAIN_SOURCE: &str = "/home/badcw/repo/icpc-cmake/bin/A.cc;/home/badcw/repo/icpc-cmake/bin/B.cc;/home/badcw/repo/icpc-cmake/bin/C.cc;/home/badcw/repo/icpc-cmake/bin/D.cc;/home/badcw/repo/icpc-cmake/bin/E.cc;/home/badcw/repo/icpc-cmake/bin/F.cc;/home/badcw/repo/icpc-cmake/bin/G.cc;/home/badcw/repo/icpc-cmake/bin/H.cc;/home/badcw/repo/icpc-cmake/bin/tmp.cc;/home/badcw/repo/icpc-cmake/main.cc";
-pub static BUNDLE_OUT_DIR: &str = "/home/badcw/repo/icpc-cmake/bin_output";
-pub static EXCEPT_DEF: &str = "badcw";
